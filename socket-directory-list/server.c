aef23e440559879f0ab743b024718349ce45b373
djairmaykon@ <djairmaykon@gmail.com>
2024-12-14 07:39:27 -0300

    wip: server finish

--- socket-directory-list/server.c
+++ socket-directory-list/server.c
@@ -8,6 +8,7 @@
 #include <stdarg.h>
 
 #define MSG_SIZE 1024
+#define CHAR_STUF "STUF"
 
 void closeSockets(int n, ...) {
     va_list sockets_fd;
@@ -25,11 +26,20 @@ void errorHandler(char* message) {
     exit(EXIT_FAILURE);
 }
 
+char* messageDestuffing(char* message){
+    char* msg_copy = message;
+    char *stuf_position_pointer = strstr(message, strcat(CHAR_STUF, CHAR_STUF));
+    int position = stuf_position_pointer - message;
+
+    
+}
+
 int main(int argc, char* argv[]) {
     int server_socket_fd, client_socket_fd,client_read_size;
-    struct sockaddr_in server_addr;
-    socklen_t server_addr_len = sizeof(server_addr);
-    char *client_message, *errorMessage, *client_message_unstuffed;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t server_addr_len = sizeof(server_addr), client_addr_len = sizeof(client_addr);
+    char *client_message, *client_message_unstuffed, *client_ip, *server_answer;
+    char *directory_name, *directory_files_list;
     
     if (argc != 3) {
         printf("Usage: %s <IP server_addr> <Port>\n", argv[0]);
@@ -62,11 +72,13 @@ int main(int argc, char* argv[]) {
     }
     printf("Server listening on %s:%d\n", ip_address, port);
 
-    client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&server_addr, &server_addr_len);
+    client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&client_addr, &client_addr_len);
     if (client_socket_fd == -1) {
         closeSockets(client_socket_fd, server_socket_fd);
         errorHandler("Could not accept client connection\n");
     }
+    client_ip = inet_ntoa(client_addr.sin_addr);
+    printf("Accept connection of client %s\n", client_ip);
 
     client_read_size = recv(client_socket_fd, client_message, MSG_SIZE - 1, 0);
     if (client_read_size < -1) {
@@ -78,8 +90,19 @@ int main(int argc, char* argv[]) {
         closeSockets(client_socket_fd, server_socket_fd);
         errorHandler("Client Message not known\n");
     }
+    printf("Client starts directory list save service.");
+
+    server_answer = "READY ACK";
+    send(client_socket_fd, server_answer, strlen(server_answer), 0);
 
     do {
+        client_read_size = recv(client_socket_fd, client_message, MSG_SIZE - 1, 0);
+        if (client_read_size < -1) {
+            closeSockets(client_socket_fd, server_socket_fd);
+            errorHandler("Could not read client message\n");
+        }
+        client_message_unstuffed = client_message
+
 
     } while(client_read_size > 0 || client_message_unstuffed != "bye");
 
