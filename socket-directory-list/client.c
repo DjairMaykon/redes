c330df1afc5581be3f40d94c3864fe7f97c7454c
djairmaykon@ <djairmaykon@gmail.com>
2025-02-04 06:00:03 -0300

    WIP: servidor do socket directory list

--- socket-directory-list/client.c
+++ socket-directory-list/client.c
@@ -5,10 +5,165 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <stdarg.h>
+#include <math.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+void closeSockets(int n, ...) {
+    va_list sockets_fd;
+    va_start(sockets_fd, n);
+    if (n > 0) {
+        for (int i = 0; i < n; i++) {
+            close(va_arg(sockets_fd, int));
+        }
+    }
+    va_end(sockets_fd);
+}
+
+void errorHandler(char* message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+char* listDirectory(char* directory_path) {
+    struct dirent *de;
+
+    DIR *dr = opendir(directory_path);
+  
+    if (dr == NULL) { 
+        errorHandler("Could not open current directory"); 
+    } 
+  
+    char *list = malloc (pow(2,16));
+    while ((de = readdir(dr)) != NULL)  {
+        if (strcmp(de->d_name, ".") && strcmp(de->d_name, "..")) {
+            strcat(list, de->d_name);
+            strcat(list, ", ");
+        }
+    }
+    closedir(dr);
+
+    return list;
+}
+
+char* messageStuffing(char* message){
+    char *dest = malloc(10255);
+    char *newMsg = malloc(10255);
+    strcpy(newMsg, message);
+    char *stuf_position_pointer = strstr(newMsg, "bye");
+    if (stuf_position_pointer == NULL) return message;
+    
+    while(stuf_position_pointer != NULL) {
+        int position = stuf_position_pointer - newMsg;
+        free(dest);
+        dest = malloc(10255);
+        strncpy(dest, newMsg, position);
+        dest[position] = '\0';
+        strcat(dest, "byye");
+        strncat(dest, newMsg + (position + 3), strlen(newMsg) - (position + 3));
+        free(newMsg);
+        newMsg = malloc(10255);
+        strcpy(newMsg, dest);
+        stuf_position_pointer = strstr(newMsg, "bye");
+    }
+
+    return dest;
+}
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <IP address> <Port> <Directory Name>\n", argv[0]);
-        return 1;
+    int server_socket_fd;
+    struct sockaddr_in server_addr;
+    char *connection_messages, *message, *message_stuffed, *server_answer;
+
+    if (argc != 5) {
+        printf("Usage: %s <IP server_addr> <Port> <Message Size> <Directory Path>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
+
+    const char *ip_address = argv[1];
+    int port = atoi(argv[2]);
+    const int MSG_SIZE = atoi(argv[3]);
+    char *directory_list = messageStuffing(listDirectory(argv[4]));
+    char *directory_path = messageStuffing(argv[4]);
+
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd == -1) {
+        errorHandler("Could not create socket\n");
+    }
+    printf("Socket created\n");
+
+    server_addr.sin_addr.s_addr = inet_addr(ip_address);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    if (connect(server_socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        closeSockets(server_socket_fd);
+        errorHandler("Connect failed. Error");
+    }
+    printf("Connected to server\n");
+
+    connection_messages = "READY";
+    if (send(server_socket_fd, connection_messages, sizeof(char)*strlen(connection_messages), 0) < 0) {
+        closeSockets(server_socket_fd);
+        errorHandler("Send failed");
+    }
+
+    if (recv(server_socket_fd, server_answer, sizeof(char)*9, 0) < 0) {
+        closeSockets(server_socket_fd);
+        errorHandler("Receive failed");
+    }
+    if (strcmp(server_answer, "READY ACK") != 0) {
+        closeSockets(server_socket_fd);
+        errorHandler("Server Message not known\n");
+    }
+    printf("Starts directory list save service.\n");
+
+    char *aux = malloc(pow(2,16)); 
+    while (strlen(directory_list) > 0) {
+        free(aux);
+        aux = malloc(pow(2,16));
+        free(message);
+        message = malloc(MSG_SIZE);
+        int size_directory_list = sizeof(char)*strlen(directory_list);
+        strncpy(message, directory_list+0, MIN(MSG_SIZE,size_directory_list));
+        message[MIN(MSG_SIZE,size_directory_list)] = '\0';
+        strncpy(aux, directory_list+MIN(MSG_SIZE,size_directory_list), strlen(directory_list) -1);
+        aux[strlen(directory_list) -1] = '\0';
+        strcpy(directory_list, aux);
+        if (send(server_socket_fd, connection_messages, strlen(connection_messages), 0) < 0) {
+            closeSockets(server_socket_fd);
+            errorHandler("Send failed");
+        }
+    }
+    printf("Send path to server: %s.\n", connection_messages);
+    
+    char *aux = malloc(pow(2,16)); 
+    while (strlen(directory_list) > 0) {
+        free(aux);
+        aux = malloc(pow(2,16));
+        free(message);
+        message = malloc(MSG_SIZE);
+        int size_directory_list = sizeof(char)*strlen(directory_list);
+        strncpy(message, directory_list+0, MIN(MSG_SIZE,size_directory_list));
+        message[MIN(MSG_SIZE,size_directory_list)] = '\0';
+        strncpy(aux, directory_list+MIN(MSG_SIZE,size_directory_list), strlen(directory_list) -1);
+        aux[strlen(directory_list) -1] = '\0';
+        strcpy(directory_list, aux);
+
+        // if (send(server_socket_fd, message, MSG_SIZE, 0) < 0) {
+        //     closeSockets(server_socket_fd);
+        //     errorHandler("Send failed");
+        // }
+        // printf("Send to server: %s\n", message);
+    }
+    connection_messages = "bye";
+    if (send(server_socket_fd, connection_messages, sizeof(connection_messages), 0) < 0) {
+        closeSockets(server_socket_fd);
+        errorHandler("Send failed");
+    }
+    printf("Finished directory list save service\n");
+
+    closeSockets(server_socket_fd);
 }
--- socket-directory-list/server.c
+++ socket-directory-list/server.c
@@ -6,9 +6,11 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <math.h>
+#include <sys/stat.h>
 
-#define MSG_SIZE 1024
-#define CHAR_STUF "STUF"
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define DIRECTORY "server-files"
 
 void closeSockets(int n, ...) {
     va_list sockets_fd;
@@ -27,31 +29,59 @@ void errorHandler(char* message) {
 }
 
 char* messageDestuffing(char* message){
-    char* msg_copy = message;
-    char *stuf_position_pointer = strstr(message, strcat(CHAR_STUF, CHAR_STUF));
-    int position = stuf_position_pointer - message;
-
+    char *dest = malloc(10255);
+    char *newMsg = malloc(10255);
+    strcpy(newMsg, message);
+    char *stuf_position_pointer = strstr(newMsg, "byye");
+    if (stuf_position_pointer == NULL) return message;
     
+    while(stuf_position_pointer != NULL) {
+        int position = stuf_position_pointer - newMsg;
+        
+        free(dest);
+        dest = malloc(10255);
+        strncpy(dest, newMsg + 0, position);
+        dest[position] = '\0';
+        strcat(dest, "bye");
+        strncat(dest, newMsg + (position + 4), strlen(newMsg) - (position + 4));
+        free(newMsg);
+        newMsg = malloc(10255);
+        strcpy(newMsg, dest);
+        stuf_position_pointer = strstr(newMsg, "byye");
+    }
+
+    return dest;
 }
 
 int main(int argc, char* argv[]) {
+    struct stat st = {0};
     int server_socket_fd, client_socket_fd,client_read_size;
     struct sockaddr_in server_addr, client_addr;
     socklen_t server_addr_len = sizeof(server_addr), client_addr_len = sizeof(client_addr);
-    char *client_message, *client_message_unstuffed, *client_ip, *server_answer;
-    char *directory_name, *directory_files_list;
+    char *client_connection_messages, *client_message, *client_message_unstuffed, *client_ip, *server_answer;
+    char *client_directory_name, *client_file_name, *client_file_path;
+    FILE *client_file;
     
-    if (argc != 3) {
-        printf("Usage: %s <IP server_addr> <Port>\n", argv[0]);
+    if (argc != 4) {
+        printf("Usage: %s <IP server_addr> <Port> <Message Size>\n", argv[0]);
         exit(EXIT_FAILURE);
+    }
+
+    if (stat(DIRECTORY, &st) == -1) {
+        if (mkdir(DIRECTORY, 0777) != 0) {
+            errorHandler("Erro ao criar o diretório");
+        }
+        printf("Diretório '%s' criado com sucesso.\n", DIRECTORY);
+    } else {
+        printf("Diretório '%s' já existe.\n", DIRECTORY);
     }
 
     const char *ip_address = argv[1];
     int port = atoi(argv[2]);
+    const int MSG_SIZE = atoi(argv[3]);
 
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket_fd == -1) {
-        closeSockets(server_socket_fd);
         errorHandler("Could not create socket\n");
     }
     printf("Socket created\n");
@@ -80,31 +110,64 @@ int main(int argc, char* argv[]) {
     client_ip = inet_ntoa(client_addr.sin_addr);
     printf("Accept connection of client %s\n", client_ip);
 
-    client_read_size = recv(client_socket_fd, client_message, MSG_SIZE - 1, 0);
-    if (client_read_size < -1) {
+    client_connection_messages = malloc(sizeof(char)*5);
+    client_read_size = recv(client_socket_fd, client_connection_messages, sizeof(char)*5, 0);
+    if (client_read_size < 0) {
         closeSockets(client_socket_fd, server_socket_fd);
         errorHandler("Could not read client message\n");
     }
-    
-    if (strcmp(client_message, "READY") != 0) {
+    if (strcmp(client_connection_messages, "READY") != 0) {
         closeSockets(client_socket_fd, server_socket_fd);
         errorHandler("Client Message not known\n");
     }
-    printf("Client starts directory list save service.");
+    printf("Starts directory list save service.\n");
+    free(client_connection_messages);
 
     server_answer = "READY ACK";
-    send(client_socket_fd, server_answer, strlen(server_answer), 0);
+    send(client_socket_fd, server_answer, sizeof(char)*strlen(server_answer), 0);
 
-    do {
-        client_read_size = recv(client_socket_fd, client_message, MSG_SIZE - 1, 0);
-        if (client_read_size < -1) {
-            closeSockets(client_socket_fd, server_socket_fd);
-            errorHandler("Could not read client message\n");
+    client_read_size = recv(client_socket_fd, client_message, pow(2,16), 0);
+    if (client_read_size < 0) {
+        closeSockets(client_socket_fd, server_socket_fd);
+        errorHandler("Could not read client message\n");
+    }
+    client_message_unstuffed = messageDestuffing(client_message);
+    strcpy(client_directory_name, client_message_unstuffed);
+    printf("Received client directory name: %s\n", client_directory_name);
+
+    client_file_name = malloc(pow(2,16));
+    strcpy(client_file_name, client_ip);
+    strcat(client_file_name, ":");
+    strcat(client_file_name, argv[2]);
+    strcat(client_file_name, "-");
+    strcat(client_file_name, client_directory_name);
+    strcat(client_file_name, ".txt");
+    client_file_path = malloc(pow(2,16));
+    strcpy(client_file_path, DIRECTORY);
+    strcat(client_file_path, "/");
+    strcat(client_file_path, client_file_name);
+    client_file = fopen(client_file_path, "w+");
+    if (client_file == NULL) {
+        errorHandler("Erro ao abrir/criar o arquivo");
+    }
+    printf("Created file %s\n", client_file_path);
+    free(client_message);
+    client_message = malloc(MAX(3*8, MSG_SIZE));
+
+    printf("%d\n", MAX(3*8, MSG_SIZE));
+    while(
+        (client_read_size = recv(client_socket_fd, client_message, 3, 0)) > 0
+    ) {
+        client_message_unstuffed = messageDestuffing(client_message);
+        printf("msg> %s\n", client_message_unstuffed);
+        if (strcmp(client_message_unstuffed, "bye") == 0) {
+            printf("Finished directory list save service\n");
+            break;
         }
-        client_message_unstuffed = client_message
 
-
-    } while(client_read_size > 0 || client_message_unstuffed != "bye");
+        fprintf(client_file, "%s", client_message_unstuffed);
+    }
 
     closeSockets(client_socket_fd, server_socket_fd);
+    fclose(client_file);
 }
